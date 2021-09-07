8e66ee6b7261af74b531e63b7efa7e56fb645cca
sha3er97 <sha3er97@gmail.com>
2021-09-07 17:21:48 +0200

    [1.0] add exit button to main screen

---
+++ cairo_bisco_app/lib/components/buttons/log_out_btn.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:cairo_bisco_app/classes/values/colors.dart';
+
+class LogOutBtn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'logOutButton',
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Icon(
+          Icons.close,
+          color: KelloggColors.darkRed,
+        ),
+      ),
+    );
+  }
+}
