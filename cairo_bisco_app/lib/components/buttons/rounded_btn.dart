c9f10ed511150288daa8ca100c5f8f2b97d80b4c
sha3er97 <sha3er97@gmail.com>
2021-11-04 13:27:29 +0200

    [1.9.8] clean code

--- cairo_bisco_app/lib/components/buttons/rounded_btn.dart
+++ cairo_bisco_app/lib/components/buttons/rounded_btn.dart
@@ -2,7 +2,8 @@ import 'package:cairo_bisco_app/classes/values/constants.dart';
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
-  RoundedButton({required this.color, required this.btnText, required this.onPressed});
+  RoundedButton(
+      {required this.color, required this.btnText, required this.onPressed});
 
   final Color color;
   final String btnText;
