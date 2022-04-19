cb317007b4278343002848a974ce9211ec347a58
sha3er97 <sha3er97@gmail.com>
2022-04-19 22:56:49 +0200

    [4.0.0] release new version to production

--- cairo_bisco_app/lib/classes/values/constants.dart
+++ cairo_bisco_app/lib/classes/values/constants.dart
@@ -3,8 +3,8 @@ import 'package:cairo_bisco_app/classes/OverWeightReport.dart';
 import 'form_values.dart';
 
 /** version configs**/
-const versionNum = '3.1.3'; //edit also in pubspec.yaml
-const versionCode = 13; //edit also in pubspec.yaml
+const versionNum = '4.0.0'; //edit also in pubspec.yaml
+const versionCode = 14; //edit also in pubspec.yaml
 const playStoreLink =
     'https://play.google.com/store/apps/details?id=com.kellogg.bisco.cairo_bisco_app';
 
